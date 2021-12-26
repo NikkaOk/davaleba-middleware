56ca0e3623b62b29341ac172c21b2904701022d4
NikkaOk <nika.okropiridze.1@btu.edu.ge>
2021-12-26 19:58:35 +0400

    commit

---
+++ app/Http/Middleware/nameCheck.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class nameCheck
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        //dd($request);
+        if($request->input('name') == "my-super-middleware"){
+            return redirect('/profile');
+        }
+        else {
+            return redirect('/error');
+        }
+    }
+}
